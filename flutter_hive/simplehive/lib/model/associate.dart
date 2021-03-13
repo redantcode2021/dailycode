5075084f3b6239abd6f8324a449ead83f79d399b
redantcode2021 <redantcyber@gmail.com>
2021-03-13 21:15:45 +0700

    Created & Generated the associate model

---
+++ flutter_hive/simplehive/lib/model/associate.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+
+part 'associate.g.dart';
+
+@HiveType(typeId: 0)
+class Associate {
+  @HiveField(0)
+  final String name;
+  @HiveField(1)
+  final int age;
+  @HiveField(2)
+  final int phone;
+  @HiveField(3)
+  final DateTime joinDate;
+  @HiveField(4)
+  final bool isSenior;
+
+  Associate({
+    @required this.name,
+    this.age,
+    this.phone,
+    this.joinDate,
+    this.isSenior,
+  });
+}
