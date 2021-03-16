cc735c9768c10763fbc5cfbe257b022cd7001b40
redantcode2021 <redantcyber@gmail.com>
2021-03-16 22:05:13 +0700

    Enabled AssociateView() component

---
+++ flutter_hive/simplehive/lib/pages/associate_view_page.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:simplehive/model/associate.dart';
+import 'package:simplehive/pages/associate_edit_page.dart';
+
+import '../associate_data.dart';
+import '../utils.dart';
+
+class AssociateView extends StatelessWidget {
+  const AssociateView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    void _deleteConfirmation(Associate currentAssociate) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              backgroundColor: Colors.white,
+              title: Text(
+                'Are you sure?',
+                style: TextStyle(color: Colors.black),
+              ),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text('You are about to delete ${currentAssociate.name}'),
+                    SizedBox(height: 10.0),
+                    Text("This action cannot be undone"),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text(
+                    "DELETE",
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    Log.d("Deleting ${currentAssociate.name}");
+                    Provider.of<AssociateData>(context, listen: false)
+                        .deleteAssociate(currentAssociate.key);
+
+                    Navigator.popUntil(context,
+                        ModalRoute.withName(Navigator.defaultRouteName));
+                  },
+                ),
+                FlatButton(
+                  child: Text("Cancel",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  onPressed: () {
+                    Log.d("Cancelling");
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          });
+    }
+
+    return Consumer<AssociateData>(builder: (context, associateData, child) {
+      Associate currentAssociate = associateData.getActiveAssociate();
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            currentAssociate?.name,
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.lightBlueAccent,
+          elevation: 16.0,
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.create),
+                iconSize: 24.0,
+                tooltip: "Edit",
+                onPressed: () {
+                  Log.d("Selected to edit");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return AssociateEditPage(
+                            currentAssociate: currentAssociate);
+                      },
+                    ),
+                  );
+                }),
+            IconButton(
+                icon: Icon(Icons.delete),
+                iconSize: 24.0,
+                color: Colors.red,
+                tooltip: "Delete",
+                onPressed: () {
+                  Log.d("Selected for deletion");
+                  _deleteConfirmation(currentAssociate);
+                })
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 36.0,
+                  color: Colors.grey[300],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(
+                        "Phone",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      Text(
+                        currentAssociate?.phone.toString(),
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 36.0,
+                  color: Colors.white54,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(
+                        "Join Date",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      Text(
+                        DateFormat("yyyy-MM-dd")
+                            .format(currentAssociate?.joinDate),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 36.0,
+                  color: Colors.grey[300],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(
+                        "Senior",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      Switch(
+                        activeTrackColor: Colors.black,
+                        value: currentAssociate.isSenior,
+                        onChanged: (bool v) {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
--- flutter_hive/simplehive/lib/widgets/associate_tile.dart
+++ flutter_hive/simplehive/lib/widgets/associate_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:simplehive/associate_data.dart';
 import 'package:simplehive/model/associate.dart';
+import 'package:simplehive/pages/associate_view_page.dart';
 
 class AssociateTile extends StatelessWidget {
   const AssociateTile({Key key, this.titleIndex}) : super(key: key);
@@ -42,7 +43,7 @@ class AssociateTile extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) {
-                //return AssociateView();
+                return AssociateView();
               }),
             );
           },
