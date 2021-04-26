3b559dee29c3acef83524fd520b03671df94fcb5
redantcode2021 <redantcyber@gmail.com>
2021-04-27 04:21:47 +0700

    Completed custom input

---
+++ iot/tempiot/lib/widgets/custom_input.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomInput extends StatelessWidget {
+  const CustomInput(
+      {Key key,
+      this.hintText,
+      this.onChanged,
+      this.onSubmitted,
+      this.focusNode,
+      this.textInputAction,
+      this.isPasswordField})
+      : super(key: key);
+
+  final String hintText;
+  final Function(String) onChanged;
+  final Function(String) onSubmitted;
+  final FocusNode focusNode;
+  final TextInputAction textInputAction;
+  final bool isPasswordField;
+
+  @override
+  Widget build(BuildContext context) {
+    bool _isPasswordField = isPasswordField ?? false;
+
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 24.0,
+        vertical: 8.0,
+      ),
+      decoration: BoxDecoration(
+        color: Color(0xFFF2F2F2),
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: TextField(
+        obscureText: _isPasswordField,
+        focusNode: focusNode,
+        onChanged: onChanged,
+        onSubmitted: onSubmitted,
+        textInputAction: textInputAction,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hintText ?? "Hint Text...",
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
+        ),
+      ),
+    );
+  }
+}
